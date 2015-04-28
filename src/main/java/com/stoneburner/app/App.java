9fe7177fe18df29baee74cbc4b43b4b7934ea971
Patrick Stoneburner <patrick.stoneburner@jivesoftware.com>
2015-04-28 10:03:47 -0700

    SSL updates

--- src/main/java/com/stoneburner/app/App.java
+++ src/main/java/com/stoneburner/app/App.java
@@ -4,7 +4,14 @@ import java.io.*;
 import org.apache.http.client.*;
 import org.apache.http.client.methods.*;
 import org.apache.http.*;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.conn.ssl.*;
+import org.apache.http.impl.conn.SingleClientConnManager;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
 
 public class App 
 {
@@ -26,13 +33,25 @@ public class App
         System.out.println( "Fetching '" + inputURI + "'");
 
         //Instantiate client and method
+        HostnameVerifier hostnameVerifier = org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+
         HttpClient client = new DefaultHttpClient();
+
+        SchemeRegistry registry = new SchemeRegistry();
+        SSLSocketFactory socketFactory = SSLSocketFactory.getSocketFactory();
+        socketFactory.setHostnameVerifier((X509HostnameVerifier) hostnameVerifier);
+        registry.register(new Scheme("https", socketFactory, 443));
+        SingleClientConnManager mgr = new SingleClientConnManager(client.getParams(), registry);
+        DefaultHttpClient httpClient = new DefaultHttpClient(mgr, client.getParams());
+
+        // Set verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(hostnameVerifier);
         HttpGet method = new HttpGet(inputURI);
 
         //Execute client with our method
         try
         {
-            HttpResponse response = client.execute(method);
+            HttpResponse response = httpClient.execute(method);
 
             System.out.println("The status: " + response.getStatusLine());
 
